2f490edafef87633addced6b3d14ca7e9e2bf08b
evanvvav <ivankolesov.fi@gmail.com>
2021-10-28 16:28:12 +0300

    first commit

---
+++ src/main/java/com/example/ohjelmistoprojekti/OhjelmistoprojektiTeamDaisJApplication.java
@@ -0,0 +1,13 @@
+package com.example.ohjelmistoprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhjelmistoprojektiTeamDaisJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhjelmistoprojektiTeamDaisJApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ohjelmistoprojekti/OhjelmistoprojektiTeamDaisJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ohjelmistoprojekti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhjelmistoprojektiTeamDaisJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
