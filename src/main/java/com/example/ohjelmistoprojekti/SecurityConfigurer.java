ed97c328a7fee4b37e776a00ed8ead6924306011
YuliaKokorieva <yuliya.pastushenko@gmail.com>
2021-12-09 10:32:50 +0200

    CORS

--- src/main/java/com/example/ohjelmistoprojekti/SecurityConfigurer.java
+++ src/main/java/com/example/ohjelmistoprojekti/SecurityConfigurer.java
@@ -32,7 +32,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
+		http.cors().and().csrf().disable()
 		.authorizeRequests().antMatchers("/authenticate").permitAll()
 				.antMatchers(HttpMethod.POST, "/apiuseranswers/**", "/savealluseranswers/**", "/apiouanswers/**", "/saveallouanswers/**", "/apirespondents/**" ).permitAll()
 				.antMatchers(HttpMethod.GET, "/apisurveys/**", "/apiquestions/**", "/apianswers/**", "/apirespondents/**").permitAll()
